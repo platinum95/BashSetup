5d0ce804d9e10425c2293c7faa4f2835cb542588
Peter Mullen <platinum95@platinum-productions.org>
2018-08-14 22:24:48 +0100

    Initial commit

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Initialise a bash environment (hopefully)
+
+# Make vim directories
+mkdir -p ~/.vim/bundle
+git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
